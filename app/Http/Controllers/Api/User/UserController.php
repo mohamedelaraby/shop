cbde92267cc934ddbb686e25e3c6e30d1106dacf
mohamedelaraby <mohamedelaraby1995@gmail.com>
2020-05-09 18:18:49 +0200

    Create new user

--- app/Http/Controllers/Api/User/UserController.php
+++ app/Http/Controllers/Api/User/UserController.php
@@ -31,7 +31,25 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // Set validate rules
+        $rules = [
+            'name'=>'required',
+            'email'=>'required|email|unique:users',
+            'password'=>'required|min:6|confirmed',
+        ];
+        //Validate  income request
+        $this->validate($request,$rules);
+        //Create user
+        $data = $request->all();
+        $data['password']=bcrypt($request->password);
+        $data['verified'] = User::UNVERIFIED_USER;
+        $data['verification_token'] = User::generateVerificationCode();
+        $data['admin'] = User::REGULAR_USER;
+
+        $user = User::create($data);
+
+        // return 200 Ok response
+        return response()->json(['data'=>$user],201);
     }
 
     /**
@@ -42,7 +60,9 @@ class UserController extends Controller
      */
     public function show($id)
     {
-        //
+        $user = User::findOrFail($id);
+
+        return response()->json(['data'=>$user],200);
     }
 
  
